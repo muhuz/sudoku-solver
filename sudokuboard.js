82a20c861d2e124b7ef2d4e4eeee7c4d09efa61a
Michael Zheng <mhzbox@gmail.com>
2020-03-14 19:17:43 -0700

    add style, class refactor, num filter

--- sudokuboard.js
+++ sudokuboard.js
@@ -1,51 +1,77 @@
 
-var NUM_ROWS = 9
-var NUM_COLS = 9
+var NUM_ROWS = 9;
+var NUM_COLS = 9;
 
 /*
-Test the Two way data binding
+Two way data binding Square
 */
 
-function Square(element, x, y, number) {
-    this.element = element;
-    this.element.value = number;
-    this.number = number;
-    this.x = x;
-    this.y = y;
-    element.addEventListener("change", this, false);
-};
+// restrict the input to only 1-9
+function setSudokuFilter(textbox) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (/^[1-9]$/.test(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    });
+  }
 
-Square.prototype.handleEvent = function (event) {
-    switch (event.type) {
-        case "change":
-            this.change(this.element.value);
+class Square {
+    constructor(element, x, y, number) {
+        this.element = element;
+        this.element.value = number;
+        this.element.addEventListener("change", this, false);
+        setSudokuFilter(this.element);
+        this.x = x;
+        this.y = y;
+        this.number = number;
     }
-};
 
-Square.prototype.change = function (number) {
-    this.number = number;
-    this.element.value = number;
-};
+    handleEvent(event) {
+        switch (event.type) {
+            case "change":
+                this.change(this.element.value);
+        };
+    }
 
-var square = new Square(document.getElementById("square"), 0, 0, null)
-console.log(square.element.value)
-
-var puzzle = document.getElementById('PuzzleBoard');
-var backend_puzzle = [];
-for (var i=0; i<9; i++) {
-    backend_puzzle[i] = new Array(9);
-};
-
-function FillPuzzle(puzzle, NUM_ROWS, NUM_COLS) {
-    for (var i = 0; i < NUM_ROWS; i++) {
-        let row = puzzle.insertRow();
-        for (let j = 0; j < NUM_COLS; j++) {
-            let cell = row.insertCell();
-            let input = document.createElement('input');
-            backend_puzzle[i][j] = new Square(input, i, j, null);
-            cell.appendChild(input);
+    change(number) {
+        // Change only if it is 1-9
+        if (number > 1 & number < 9 ) {
+            this.number = number;
+            this.element.value = number;
         }
     }
 };
 
-FillPuzzle(puzzle, 9, 9);
+class SudokuBoard {
+    constructor(table, num_rows, num_cols) {
+        this.table = table;
+        this.grid = [];
+        for (let i=0; i<num_rows; i++) {
+            this.grid[i] = new Array(num_cols);
+        };
+
+        for (var i = 0; i < num_rows; i++) {
+            let row = this.table.insertRow();
+            for (let j = 0; j < num_cols; j++) {
+                let cell = row.insertCell();
+                let input = document.createElement('input');
+                input.className = "square"
+                this.grid[i][j] = new Square(input, i, j, null);
+                cell.appendChild(input);
+            }
+        }
+    }
+};
+
+var puzzle_table = document.getElementById('PuzzleBoard');
+var board = new SudokuBoard(puzzle_table, NUM_ROWS, NUM_COLS);
+
