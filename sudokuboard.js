85a2801eab6684b0de7023325c46b405a13c0a6f
Michael Zheng <mhzbox@gmail.com>
2020-03-15 23:08:15 -0700

    add arrow key nav functionality

--- sudokuboard.js
+++ sudokuboard.js
@@ -8,67 +8,105 @@ Two way data binding Square
 
 // restrict the input to only 1-9
 function setSudokuFilter(textbox) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-      textbox.addEventListener(event, function() {
-        if (/^[1-9]$/.test(this.value)) {
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        } else {
-          this.value = "";
+    textbox.addEventListener("keydown", function(e) {
+        if (/^[1-9]$/.test(e.key)) {
+            this.value = e.key;
+            e.preventDefault();
+        } else if (e.key != 'Backspace'){
+            e.preventDefault();
         }
-      });
     });
-  }
+}
+
+// Add arrow key navigation to document when square is selected
+document.addEventListener("keydown", function(e) {
+    if (document.activeElement.className == 'square') {
+        let active_x = document.activeElement.x;
+        let active_y = document.activeElement.y;
+        if (e.keyCode == '37') {
+            // left arrow
+            if (active_y > 0) {
+                board.grid[active_x][active_y - 1].element.focus();
+            }
+            else {
+                board.grid[active_x][8].element.focus();
+            }
+        } else if (e.keyCode == '38') {
+            // up arrow
+            if (active_x > 0) {
+                board.grid[active_x - 1][active_y].element.focus();
+            }
+            else {
+                board.grid[8][active_y].element.focus();
+            }
+        } else if (e.keyCode == '39') {
+            // right arrow
+            if (active_y < 8) {
+                board.grid[active_x][active_y + 1].element.focus();
+            }
+            else {
+                board.grid[active_x][0].element.focus();
+            }
+
+        } else if (e.keyCode == '40') {
+            // down arrow
+            if (active_x < 8) {
+                board.grid[active_x + 1][active_y].element.focus();
+            }
+            else {
+                board.grid[0][active_y].element.focus();
+            }
+        }
+    }
+});
 
 class Square {
     constructor(element, x, y, number) {
         this.element = element;
-        this.element.value = number;
-        this.element.addEventListener("change", this, false);
+        this.element.x = x;
+        this.element.y = y;
+        // this.element.update(number);
+        this.element.addEventListener("change", this.handleEvent);
         setSudokuFilter(this.element);
-        this.x = x;
-        this.y = y;
         this.number = number;
     }
 
     handleEvent(event) {
         switch (event.type) {
             case "change":
-                this.change(this.element.value);
+                this.change(this.element.textContent);
         };
     }
 
     change(number) {
-        // Change only if it is 1-9
-        if (number > 1 & number < 9 ) {
-            this.number = number;
-            this.element.value = number;
-        }
+        this.number = number;
+        this.element.textContent = number;
     }
 };
 
 class SudokuBoard {
     constructor(table, num_rows, num_cols) {
         this.table = table;
+        this.table.addEventListener("click", function(e) {
+            console.log(e.toElement);
+            console.log(e.toElement.y);
+        })
         this.grid = [];
         for (let i=0; i<num_rows; i++) {
             this.grid[i] = new Array(num_cols);
         };
 
+        // initialize the table
         for (var i = 0; i < num_rows; i++) {
             let row = this.table.insertRow();
             for (let j = 0; j < num_cols; j++) {
                 let cell = row.insertCell();
                 let input = document.createElement('input');
-                input.className = "square"
+                input.className = "square";
                 this.grid[i][j] = new Square(input, i, j, null);
                 cell.appendChild(input);
             }
-        }
+        };
     }
 };
 
